3733231d2f1b2e19e4232a325d91dec47537598f
waadelessawy <82984314+waadelessawy@users.noreply.github.com>
2025-08-16 01:25:44 +0300

    Add project files.

---
+++ DesignPatterns/Program.cs
@@ -0,0 +1,53 @@
+﻿using DesignPatterns;
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //1- Singleton 
+        //Singleton s1 = Singleton.GetInstance();
+        //Singleton s2 = Singleton.GetInstance();
+
+        //if (s1 == s2)
+        //{
+        //    Console.WriteLine("Singleton works, both variables contain the same instance.");
+        //}
+        //else
+        //{
+        //    Console.WriteLine("Singleton failed, variables contain different instances.");
+        //}
+
+
+        //Singleton thread safe
+        Console.WriteLine(
+                "{0}\n{1}\n\n{2}\n",
+                "If you see the same value, then singleton was reused (yay!)",
+                "If you see different values, then 2 singletons were created (booo!!)",
+                "RESULT:"
+            );
+
+        Thread p1 = new Thread(() =>
+        {
+            TestSingleton("FOO");
+        });
+        Thread p2 = new Thread(() =>
+        {
+            TestSingleton("BAR");
+        });
+
+        p1.Start();
+        p2.Start();
+
+        p1.Join(); 
+        p2.Join();
+
+
+        static void TestSingleton(string value)
+        {
+            SingletonThreadSafe singleton = SingletonThreadSafe.GetInstance(value);
+            Console.WriteLine(singleton.Value);
+        }
+
+    }
+}
---
+++ DesignPatterns/Singleton.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns
+{
+    //Creational design pattern
+    public sealed class Singleton
+    {
+        private Singleton() { }
+
+        private static Singleton _instance;
+
+        public static Singleton GetInstance()
+        {
+            if(_instance == null)
+            {
+                _instance = new Singleton();
+            }
+            return _instance;
+        }
+        
+    }
+
+
+    //Thread-safe Singleton
+
+    public sealed class SingletonThreadSafe
+    {
+        private SingletonThreadSafe() { }
+
+        private static SingletonThreadSafe _instance;
+
+        //will be used to synchronnize threads
+        private static readonly object _lock = new object();
+
+        public static SingletonThreadSafe GetInstance(string value)
+        {
+            if(_instance == null)
+            {
+                lock (_lock)
+                {
+                    if(_instance == null)
+                    {
+                        _instance = new SingletonThreadSafe();
+                        _instance.Value = value;
+
+                    }
+                }
+            }
+            return _instance;
+        }
+
+        // We'll use this property to prove that our Singleton really works.
+        public string Value { get; set; }
+    }
+
+
+}
