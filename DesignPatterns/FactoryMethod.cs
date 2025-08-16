14a7aa71fb8991a167de51e4bafcd10f30197bdb
waadelessawy <82984314+waadelessawy@users.noreply.github.com>
2025-08-16 13:17:21 +0300

    factory method

---
+++ DesignPatterns/FactoryMethod.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns
+{
+    //Creational design pattern
+
+    public interface INotification
+    {
+        void Send(string message);
+    }
+
+    public class EmailNotification : INotification
+    {
+        public void Send(string message)
+        {
+            Console.WriteLine("Sending Email Notification: " + message);
+
+        }
+    }
+    public class SmsNotification : INotification
+    {
+        public void Send(string message)
+        {
+            Console.WriteLine("Sending SMS Notification: " + message);
+        }
+    }
+    public class PushNotification : INotification
+    {
+        public void Send(string message)
+        {
+            Console.WriteLine("Sending Push Notification: " + message);
+        }
+    }
+
+    //Create an abstract class with a factory method
+    public abstract class NotificationCreator
+    {
+
+        //factor method => create the object inside it
+        public abstract INotification CreateNotification();
+        public void Notify(string message)
+        {
+            INotification notification = CreateNotification();
+            notification.Send(message);
+        }
+    }
+
+    //Implement the factory method in concrete subclasses
+
+    public class EmailNotificatonCreator : NotificationCreator
+    {
+        public override INotification CreateNotification()
+        {
+            return new EmailNotification(); 
+        }
+    }
+
+    public class SmsNotificationCreator : NotificationCreator
+    {
+        public override INotification CreateNotification()
+        {
+            return new SmsNotification();
+        }
+    }
+    public class PushNotificationCreator : NotificationCreator
+    {
+        public override INotification CreateNotification()
+        {
+            return new PushNotification();
+        }
+    }
+    public class FactoryMethod
+    {
+
+    }
+}
--- DesignPatterns/Program.cs
+++ DesignPatterns/Program.cs
@@ -18,36 +18,53 @@ class Program
         //    Console.WriteLine("Singleton failed, variables contain different instances.");
         //}
 
+        ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
         //Singleton thread safe
-        Console.WriteLine(
-                "{0}\n{1}\n\n{2}\n",
-                "If you see the same value, then singleton was reused (yay!)",
-                "If you see different values, then 2 singletons were created (booo!!)",
-                "RESULT:"
-            );
 
-        Thread p1 = new Thread(() =>
-        {
-            TestSingleton("FOO");
-        });
-        Thread p2 = new Thread(() =>
-        {
-            TestSingleton("BAR");
-        });
+        //Console.WriteLine(
+        //        "{0}\n{1}\n\n{2}\n",
+        //        "If you see the same value, then singleton was reused (yay!)",
+        //        "If you see different values, then 2 singletons were created (booo!!)",
+        //        "RESULT:"
+        //    );
 
-        p1.Start();
-        p2.Start();
+        //Thread p1 = new Thread(() =>
+        //{
+        //    TestSingleton("FOO");
+        //});
+        //Thread p2 = new Thread(() =>
+        //{
+        //    TestSingleton("BAR");
+        //});
 
-        p1.Join(); 
-        p2.Join();
+        //p1.Start();
+        //p2.Start();
+
+        //p1.Join(); 
+        //p2.Join();
 
 
-        static void TestSingleton(string value)
-        {
-            SingletonThreadSafe singleton = SingletonThreadSafe.GetInstance(value);
-            Console.WriteLine(singleton.Value);
-        }
+        //static void TestSingleton(string value)
+        //{
+        //    SingletonThreadSafe singleton = SingletonThreadSafe.GetInstance(value);
+        //    Console.WriteLine(singleton.Value);
+        //}
+
+        ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        ///
+
+        //3- Factory Method
+
+        NotificationCreator emailCreator = new EmailNotificatonCreator();
+        emailCreator.Notify("Email message content");
+
+
+        NotificationCreator smsCreator = new SmsNotificationCreator();
+        smsCreator.Notify("SMS message content.");
+
+        NotificationCreator pushCreator = new PushNotificationCreator();
+        pushCreator.Notify("Push message content.");
 
     }
 }
